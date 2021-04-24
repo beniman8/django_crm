e516bffc2876beaec21653a4dacd1fab2766f80f
beniman8 <benik@live.ca>
2021-04-23 21:44:29 -0400

    fixed the home list and the detail view

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,11 @@
-from .views import home_page
+from .views import home_page,lead_detail
 from django.urls import path
 
 
 app_name='leads'
 
 urlpatterns = [
-    path('',home_page,name='home')
+    path('',home_page,name='home'),
+    path('<pk>/',lead_detail,name='detail-view')
     
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 
 def home_page(request):
+    leads = Lead.objects.all()
     context={
+        'leads':leads,
 
     }
 
     return render(request, 'leads/home.html', context)
+
+
+def lead_detail(request,pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        'lead':lead,
+    }
+    return render(request, 'leads/detail.html', context)
