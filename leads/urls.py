07d669cdb4c6b8ce996b3553185edfa0b39f0ece
beniman8 <benik@live.ca>
2021-04-24 11:03:04 -0400

    created forms and used it on the create view

--- leads/urls.py
+++ leads/urls.py
@@ -1,4 +1,4 @@
-from .views import home_page,lead_detail
+from .views import home_page,lead_detail,lead_create
 from django.urls import path
 
 
@@ -6,6 +6,7 @@ app_name='leads'
 
 urlpatterns = [
     path('',home_page,name='home'),
-    path('<pk>/',lead_detail,name='detail-view')
+    path('create/',lead_create,name='create-view'),
+    path('<int:pk>/',lead_detail,name='detail-view'),
     
 ]
