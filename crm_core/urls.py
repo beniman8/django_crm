dcea5abb6e8d4294ecf787b999b0f1cc61a5687d
beniman8 <benik@live.ca>
2021-04-25 10:47:19 -0400

    added tailwindcss to the project

--- crm_core/urls.py
+++ crm_core/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from leads.views import landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('landing/',landing_page,name='landing_page'),
     path('', include('leads.urls',namespace='leads')),
     
 ]
