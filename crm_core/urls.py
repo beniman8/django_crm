e897c6146bdd3ab576e6a44c14c2a7f3d01f947c
beniman8 <benik@live.ca>
2021-04-26 08:50:10 -0400

    setup the static files

--- crm_core/urls.py
+++ crm_core/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path,include
 from leads.views import landing_page,LandingPageView
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,8 @@ urlpatterns = [
     path('landing/',LandingPageView.as_view(),name='landing_page'),
     path('', include('leads.urls',namespace='leads')),
     
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
