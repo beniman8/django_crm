aaf917ec9004c7a39e37bab520a6f98ffb94301f
beniman8 <benik@live.ca>
2021-04-27 09:02:14 -0400

    quick setup of test

--- leads/tests.py
+++ leads/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_get(self):
+        response = self.client.get(reverse('landing_page'))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,"landing.html")
+        
+
+
+
+    
+        
